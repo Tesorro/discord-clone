42c158ff7258fdd4e123d408fc544f49729d6a29
Zaur <z.tsoroev@original-group.ru>
2024-11-06 21:08:13 +0300

    added popover from shadcn, added emoji bar

--- src/components/chat/chat-input.tsx
+++ src/components/chat/chat-input.tsx
@@ -7,8 +7,10 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { Plus, Smile } from 'lucide-react';
+import { Plus } from 'lucide-react';
 import { useModal } from '@/hooks/use-modal-store';
+import { EmojiPicker } from '@/components/emoji-picker';
+import { useRouter } from 'next/navigation';
 
 interface ChatInputProps {
   apiUrl: string;
@@ -23,6 +25,7 @@ const formSchema = z.object({
 
 export const ChatInput = ({ apiUrl, query, type, name }: ChatInputProps) => {
   const { onOpen } = useModal();
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -36,6 +39,9 @@ export const ChatInput = ({ apiUrl, query, type, name }: ChatInputProps) => {
     try {
       const url = qs.stringifyUrl({ url: apiUrl, query });
       await axios.post(url, values);
+
+      form.reset();
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
@@ -68,7 +74,11 @@ export const ChatInput = ({ apiUrl, query, type, name }: ChatInputProps) => {
                     {...field}
                   />
                   <div className={'absolute top-7 right-8'}>
-                    <Smile className={'cursor-pointer'} />
+                    <EmojiPicker
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value} ${emoji}`)
+                      }
+                    />
                   </div>
                 </div>
               </FormControl>
